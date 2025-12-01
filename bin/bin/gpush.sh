d4249b9b71887001890fed14295df3a26b90e556
angelogreko <angelogreko@gmail.com>
2025-12-01 15:18:11 +0200

    save

--- bin/bin/gpush.sh
+++ bin/bin/gpush.sh
@@ -3,13 +3,6 @@
   wget -q --spider http://google.com
 
   if [ $? -eq 0 ]; then
-#	cd  ~/.config/FreeTube/
-#	rsync history.db  ~/dotfiles/freetube/.config/FreeTube/history.db
-#	rsync playlists.db  ~/dotfiles/freetube/.config/FreeTube/playlists.db
-#	rsync profiles.db  ~/dotfiles/freetube/.config/FreeTube/profiles.db
-#	rsync search-history.db  ~/dotfiles/freetube/.config/FreeTube/search-history.db
-#	rsync settings.db  ~/dotfiles/freetube/.config/FreeTube/settings.db
-
 	~/bin/gstatus.sh
 	
 	cd ~/vd
