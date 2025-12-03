45ea59f28d34108e151fa6a7b5c9cc8ef08cb4f3
angelogreko <angelogreko@gmail.com>
2025-12-04 01:11:25 +0200

    commit

--- dwl/.config/dwl/autostart.sh
+++
@@ -1,6 +0,0 @@
-pgrep pipewire > /dev/null && pkill pipewire
-pipewire &
-waybar &
-nm-applet &
-foot &
-pgrep swaybg > /dev/null || swaybg -m fit -i ~/.config/backgrounds/sunparth.jpg 
--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -46,7 +46,7 @@ alias cdd='cd ~/Downloads/'
 alias cdc='cd /mnt/data/'
 alias cg='cd ~/git/'
 alias cdf='cd ~/vd/'
-alias cdw='cd ~/dwl'
+alias cdw='cd ~/vd/dl'
 
 # yt-dlp
 alias yt="yt-dlp -x --audio-format mp3 -o='~/Music/%(title)s.%(ext)s'"
@@ -149,4 +149,4 @@ alias ph='~/bin/phonebup.sh'
 
 alias dd='~/bin/dd.sh'
 
-alias ncc='nano ~/dwl/config.def.h'
+alias ncc='nano ~//vd/dl/config.def.h'
